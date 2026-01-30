257435aa2186203728763ddb4783cc9b1a285bcd
thomashcw <thomas.helfenberger@contoweb.ch>
2026-01-30 13:41:23 +0100

    Fix style issue

--- src/Models/v1/Stock.php
+++ src/Models/v1/Stock.php
@@ -7,5 +7,6 @@ use Contoweb\AbacusApi\Models\AbacusModel;
 class Stock extends AbacusModel
 {
     protected static string $resource = 'Stocks';
+
     protected static string|array $primaryKey = ['ProductId', 'VariantId', 'StockLocation', 'StockArea'];
 }
