3c3f6714f94f35c7288e9d653a2d7e6852cb9dac
thomashcw <thomas.helfenberger@contoweb.ch>
2026-02-17 09:32:14 +0100

    remove unnecessary constructor

--- src/Exceptions/MissingCredentialsException.php
+++ src/Exceptions/MissingCredentialsException.php
@@ -4,10 +4,4 @@ namespace Contoweb\AbacusApi\Exceptions;
 
 use Exception;
 
-class MissingCredentialsException extends Exception
-{
-    public function __construct($message = '', $code = 0, $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
+class MissingCredentialsException extends Exception {}
